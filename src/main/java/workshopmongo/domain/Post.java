cb8ef601603c8572c1d88c00e968d31039d1b7fe
Roberto Pacheco Claro <robertopacheco1994@gmail.com>
2023-04-10 12:00:14 -0300

    Acrescentando comentarios aos posts

--- src/main/java/workshopmongo/domain/Post.java
+++ src/main/java/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import workshopmongo.dto.AuthorDTO;
+import workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{	
@@ -19,12 +22,12 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
-
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
-		super();
 	}
-	
+
 	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
@@ -74,9 +77,20 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -88,7 +102,11 @@ public class Post implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Post other = (Post) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-		
 }
